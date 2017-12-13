1243f7adc18f640bbf3ff937c91d21eacd84aeaa
lovelace computer <aromero18@weldre4.org>
2017-12-13 10:57:35 -0700

    first draft of red autonomous. code for color sensing is commented out as servo cannot be securely attached to

--- new app master/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/holonomicmk1.java
+++ new app master/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/holonomicmk1.java
@@ -41,20 +41,10 @@ import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.ftccommon.ClassManagerFactory;
 import com.qualcomm.robotcore.hardware.CRServo;
 import java.math.*;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
-/**
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
+
+
 
 @TeleOp(name="holonomicmk1", group="teliop")
 
@@ -73,6 +63,7 @@ public class holonomicmk1 extends OpMode
 
     public Servo claw_front_left = null;
     public Servo claw_front_right = null;
+
 
        /*
      * Code to run ONCE when the driver hits INIT
